37acc29f7df1e76e639005c3dfa57a7c435459ca
taehopark <qkrxogh7@gmail.com>
2022-10-21 10:58:34 +0900

    login del

--- accounts/views.py
+++ accounts/views.py
@@ -22,7 +22,3 @@ def signup(request):
         'form':form
     }
     return render(request,'accounts/signup.html',context)
-
-def login(request):
-    if request.method == 'POST':
-        form = AuthentionForm
