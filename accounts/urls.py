28f657f4e36a9e586fe9cef925ce845ae8bfbcbf
taehopark <qkrxogh7@gmail.com>
2022-10-21 13:40:09 +0900

    nav

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,9 +5,9 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('',views.index,name='index'),
-    path('signup',views.signup,name='signup'),
-    path('login',views.login,name='login'),
-    path('logout',views.logout,name='logout'),
+    path('signup/',views.signup,name='signup'),
+    path('login/',views.login,name='login'),
+    path('logout/',views.logout,name='logout'),
     ]
 
 
